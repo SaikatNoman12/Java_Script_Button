0214997aad7ce5cfd54378870c5abdbbafa158da
SaikatNoman12 <noommaa240@gmail.com>
2022-02-09 10:36:28 +0600

    complete 7th type button

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 
 function buttonBlue(){
     const blue = document.body.style.background = "blue";
-}
+};
 
 
 // button on click type - 3 colour "green"
@@ -16,7 +16,7 @@ green.onclick = greenButton;
 
 function greenButton(){
     document.body.style.background = "green"
-}
+};
 
 
 // button onclick type - 4  colour "yellow"
@@ -25,7 +25,7 @@ const yellow = document.getElementById("make-button-yellow");
 
 yellow.onclick = function (){
 	document.body.style.background = "yellow";
-}
+};
 
 
 // button onclick use addEventListener type - 5 colour "gray"
@@ -44,12 +44,12 @@ const brown = document.getElementById("make-button-brown");
 
 brown.addEventListener('click', function (){
     document.body.style.background = "brown";
-})
+});
 
 // button onclick use addEventListener type - 7 "pink"
 
 document.getElementById("make-button-pink").addEventListener('click', function (){
-    document.body.style.background = "pink"
+    document.body.style.background = "pink";
 });
 
 
