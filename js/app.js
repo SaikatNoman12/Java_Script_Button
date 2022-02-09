5b63fa986cae1158450377eaca927f8144e31116
SaikatNoman12 <noommaa240@gmail.com>
2022-02-09 10:00:07 +0600

    add 2 type on click  button

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+// button on click type - 1
+
+
+// button on click type - 2
+
+function buttonBlue(){
+    const blue = document.body.style.background = "blue";
+}
+
+
+// button on click type - 3
+const green = document.getElementById("make-button-green");
+green.onclick = greenButton; 
+
+function greenButton(){
+    document.body.style.background = "green"
+}
+
+
+// button on click type - 4
+
+
