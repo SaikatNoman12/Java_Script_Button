8758681de4c71982971c6d0562d4aaeb836a816a
SaikatNoman12 <noommaa240@gmail.com>
2022-02-09 10:28:13 +0600

    7 type on click button complete

--- js/app.js
+++ js/app.js
@@ -2,14 +2,15 @@
 // button on click type - 1
 
 
-// button on click type - 2
+// button on click type - 2 colour "blue"
 
 function buttonBlue(){
     const blue = document.body.style.background = "blue";
 }
 
 
-// button on click type - 3
+// button on click type - 3 colour "green"
+
 const green = document.getElementById("make-button-green");
 green.onclick = greenButton; 
 
@@ -18,6 +19,37 @@ function greenButton(){
 }
 
 
-// button on click type - 4
+// button onclick type - 4  colour "yellow"
+
+const yellow = document.getElementById("make-button-yellow");
+
+yellow.onclick = function (){
+	document.body.style.background = "yellow";
+}
+
+
+// button onclick use addEventListener type - 5 colour "gray"
+
+const gray = document.getElementById("make-button-gray");
+gray.addEventListener('click', buttuonGray);
+
+function buttuonGray(){
+	document.body.style.background = "gray";
+};
+
+
+// button onclick use addEventListener type - 6 "brown"
+
+const brown = document.getElementById("make-button-brown");
+
+brown.addEventListener('click', function (){
+    document.body.style.background = "brown";
+})
+
+// button onclick use addEventListener type - 7 "pink"
+
+document.getElementById("make-button-pink").addEventListener('click', function (){
+    document.body.style.background = "pink"
+});
 
 
